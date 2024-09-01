a53efa80baa7d9b885299095ffa8897d20a6f61d
Mic <mic.guibert@free.fr>
2024-09-01 18:19:13 +0200

    fix: reduced main map size

--- globals/player_state.gd
+++ globals/player_state.gd
@@ -21,7 +21,7 @@ var damage_cooldown_s: float = 1.0
 # === DASH ===
 var dash_duration_s: float = 0.25
 var dash_cooldown_s: float = 3.0
-var dash_speed_multiplier: float = 5
+var dash_speed_multiplier: float = 3
 var is_dashing = false
 var can_dash = true
 var dash_gauge_value: int = 5 # [0..5] where 0 = empty and 5 = full
