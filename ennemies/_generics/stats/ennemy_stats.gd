9665dc4c74d0da02ee73f6e5b739f9d4a5eafdec
Mic <mic.guibert@ik.me>
2025-07-14 14:11:59 +0200

    refactor: added groups in resources properties

--- ennemies/_generics/stats/ennemy_stats.gd
+++ ennemies/_generics/stats/ennemy_stats.gd
@@ -1,10 +1,15 @@
 extends Resource
 class_name EnnemyStats
 
+@export_group("Display")
 @export var name = "ennemy"
 @export var nice_name = "ennemy nice name"
+
+@export_group("Attributes")
 @export var max_health: int = 30
-@export var speed: float = 200.0
 @export var xp_value: float = 1.0
 @export var can_die: bool = true
+
+@export_group("Behavior")
+@export var speed: float = 200.0
 @export var chase_player: bool = true
--- equipment/guns/_generics/stats/gun_stats.gd
+++ equipment/guns/_generics/stats/gun_stats.gd
@@ -1,15 +1,18 @@
 extends Resource
 class_name GunStats
 
+@export_group("Display")
 @export var name = "gun"
 @export var display_name = "gun"
 
+@export_group("Attack")
 @export var shots_per_s = 1.0
-
 @export var bullets_spread_angle_deg: int = 0
 @export var bullets_per_shot: int = 1
 
-@export var eject_cartridges: bool = true
-
+@export_group("Sound")
 @export var shoot_sound: String = "shoot.wav"
 @export var shoot_sfx_options: SfxOptions = preload("res://globals/sound/default_sfx_options.tres")
+
+@export_group("Effects")
+@export var eject_cartridges: bool = true
