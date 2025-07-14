23171b87cf74e1908ccd913d8fb47f950d0dcf2f
Mic <mic.guibert@ik.me>
2025-07-14 15:59:34 +0200

    feat: added splashscreen + created game theme

---
+++ scenes/splash_screen.gd
@@ -0,0 +1,25 @@
+extends Control
+
+var fade_in_time: float = 0.5
+var fade_out_time: float = 0.5
+var splash_duration: float = 3.0
+var skip_splash: bool = !OS.is_debug_build()
+
+func _ready() -> void:
+    if skip_splash:
+        SceneManager.switch_to("res://ui/menu/main_menu.tscn")
+
+    %VBoxContainer.modulate = Color.TRANSPARENT
+    create_tween().tween_property(%VBoxContainer, "modulate", Color.WHITE, fade_in_time)
+    get_tree().create_timer(splash_duration - fade_out_time).connect("timeout", leave_splash)
+    %StudioName.visible_ratio = 0.0
+    create_tween().tween_property(%StudioName, "visible_ratio", 1.0, fade_in_time * 2)
+
+func _input(event: InputEvent) -> void:
+    if event.is_pressed():
+        leave_splash()
+
+func leave_splash():
+    create_tween().tween_property(%VBoxContainer, "modulate", Color.TRANSPARENT, fade_out_time).connect("finished", func():
+        SceneManager.switch_to("res://ui/menu/main_menu.tscn")
+    )
--- ui/menu/main_menu.gd
+++ ui/menu/main_menu.gd
@@ -1,7 +1,11 @@
-extends CanvasLayer
+extends Control
 
 func _ready() -> void:
     %MusicToggle.button_pressed = SoundPlayer.enable_music
+    %SubTitle.visible_ratio = 0.0
+    create_tween().tween_property(%SubTitle, "visible_ratio", 1.0, 0.5)
+    %SubTitle.modulate = Color.RED
+    create_tween().tween_property(%SubTitle, "modulate", Color.WHITE, 0.75)
 
 func _on_start_button_pressed() -> void:
     Sounds.start_game()
@@ -12,3 +16,7 @@ func _on_music_toggle_toggled(toggled_on: bool) -> void:
 
 func _on_music_toggle_button_down() -> void:
     Sounds.click()
+
+
+func _on_quit_button_pressed() -> void:
+    get_tree().quit()
