569328ab9b4a3ad59016cc698293778323b93bfe
Mic <mic.guibert@ik.me>
2025-07-18 20:18:20 +0200

    feat: collectible items vanish after a certain amount of time

--- equipment/collectible_item.gd
+++ equipment/collectible_item.gd
@@ -1,8 +1,16 @@
 extends StaticBody2D
 class_name CollectibleItem
 
+@export var show_time = 20.0
+
 func _ready() -> void:
     Minimap.track(self, Minimap.ObjectType.COLLECTIBLE)
+    get_tree().create_timer(show_time).connect("timeout", _on_show_time_timeout)
 
 func _exit_tree() -> void:
     Minimap.untrack(self)
+
+func _on_show_time_timeout():
+    get_tree().create_tween().tween_property(self, "modulate", Color.TRANSPARENT, 0.25).connect("finished", func():
+        queue_free()
+    )
