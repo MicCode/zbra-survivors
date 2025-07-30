18fe722de9a221307e4930a65f70697625e635ef
Mic <mic.guibert@ik.me>
2025-07-30 17:22:31 +0200

    test: added a screenshot button

--- globals/utils/scene_manager.gd
+++ globals/utils/scene_manager.gd
@@ -4,8 +4,25 @@ var current_scene = null
 var transition_time = 0.5
 
 func _ready() -> void:
+    process_mode = Node.PROCESS_MODE_ALWAYS
     var root = get_tree().root
     current_scene = root.get_child(root.get_child_count() - 1)
+
+func _input(event):
+    if event.is_action_pressed("screenshot"):
+        take_screenshot()
+
+func take_screenshot():
+    await RenderingServer.frame_post_draw # Attend la fin du rendu
+    var image: Image = get_viewport().get_texture().get_image()
+    var datetime = Time.get_datetime_string_from_system().replace(":", "-")
+    var path = "user://screenshot_%s.png" % datetime
+    var response = image.save_png(path)
+
+    if response == OK:
+        print("Saved screenshot:", path)
+    else:
+        push_error("Failed to save screenshot: got response code [%d]" % response)
 
 func switch_to(scene_path: String) -> void:
     current_scene.process_mode = Node.PROCESS_MODE_DISABLED
